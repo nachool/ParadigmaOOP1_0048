20a5a41f39f98485b4c6a2835d6be52188cd075c
nachool <nachma.olivia1394@gmail.com>
2024-05-21 22:47:14 +0700

    menambah agregasi

---
+++ agregasi/anak.h
@@ -0,0 +1,15 @@
+#ifndef ANAK_H
+#define ANAK_H
+using namespace std;
+
+class anak {
+public:
+	string nama;
+	anak(string pNama) : nama(pNama) {
+		cout << "Anak \"" << nama << "\" ada \n";
+	}
+	~anak() {
+		cout << "Anak \"" << nama << "\" tidak ada \n";
+	}
+};
+#endif
