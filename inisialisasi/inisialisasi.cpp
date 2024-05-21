b8ff6cd936f713ac58cc7794b8a33cfadafecdd2
nachool <nachma.olivia1394@gmail.com>
2024-05-21 17:44:13 +0700

    menambah akses modifier public

--- inisialisasi/inisialisasi.cpp
+++ inisialisasi/inisialisasi.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
-class mahasiswa {
+class mahasiswa { //membuat class mahasiswa
+
+private:
+	const int id; //3 variabel mahasiswa
+	string nama;
+	float nilai;
+
+
 
 };
 
